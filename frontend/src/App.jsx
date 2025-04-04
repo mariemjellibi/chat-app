ce037f233115efea171aa04545157ab9014ff49a
mariemjellibi <mariemjellibi4@gmail.com>
2025-04-04 03:58:31 +0100

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
-const socket = io("http://localhost:3000");
+const socket = io("https://chat-app-6a63.onrender.com");
 
 const App = () => {
   const [username, setUsername] = useState("");
